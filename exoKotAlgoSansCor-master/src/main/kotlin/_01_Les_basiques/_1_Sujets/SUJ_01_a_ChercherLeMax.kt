a2f40c033f2606379d23ff505881d8f48d75e5aa
UgoFaye <faye.ugo77@gmail.com>
2024-09-12 14:01:01 +0200

    commit 2

--- exoKotAlgoSansCor-master/src/main/kotlin/_01_Les_basiques/_1_Sujets/SUJ_01_a_ChercherLeMax.kt
+++ exoKotAlgoSansCor-master/src/main/kotlin/_01_Les_basiques/_1_Sujets/SUJ_01_a_ChercherLeMax.kt
@@ -17,7 +17,13 @@ import _10_Demos_et_tests.testerEnLot
  */
 fun firstMax(list: List<Int>): List<Int> {
     var iMax = 0
-    // A COMPLETER ICI
+    var vmax = list[0]
+    for (i in 0..list.size -1){
+        if (list[i] > vmax){
+            vmax = list[i]
+            iMax = i
+        }
+    }
     return  listOf(list[iMax], iMax)
 }
 
